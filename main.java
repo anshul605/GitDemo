3fd6ebe475bd12ad2ae9fb64bbedab453fea5161
anshul605 <anshulsharma605@gmail.com>
2024-04-15 20:19:02 +0530

    Added interest calculator

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(Calculator.add(19,15));
+        System.out.println(InterestCalculator.calculate(1000000,8,5));
     }
 }
 
